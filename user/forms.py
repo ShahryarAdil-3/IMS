c0210382f6b38f21aca888a764acaa4996141f3b
ShahryarAdil-3 <saaman2009@gmail.com>
2025-07-10 18:55:11 +0500

    Create forms.py

---
+++ user/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm as BaseUserCreationForm
+from .models import (User)
+
+class CustomAuthenticationForm(AuthenticationForm):
+    username = forms.EmailField(widget= forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Enter your email'}))
+    password = forms.CharField(widget= forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Enter your email'}))
+
+class UserRegistrationForm(BaseUserCreationForm):
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Enter your email'}))
+    password = forms.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Enter your password'}))
+
+    class Meta:
+        model = User
+        fields = ('email', 'password')
+        widgets = {
+            'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Enter your email'}),
+        }
+
+        def save(self, commit=True):
+            user = super().save(commit=False)
+            user.email = self.cleaned_data['email']
+            if commit:
+                user.save()
+            return user
